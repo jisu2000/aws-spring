ba12f9f41729fd150e689ad8c47078ddea3e0cd4
jisu2000 <spjisu2000@gmail.com>
2024-02-04 14:42:59 +0530

    first commit

---
+++ src/test/java/com/jisu/awsspring/AwsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jisu.awsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
